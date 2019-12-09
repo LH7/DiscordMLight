e119855712d867ccd12ca6baede4ff7af0d8513c
LH7 <lh147@yandex.ru>
2019-12-09 14:07:13 +0300

    ssPanel v0.1.5 Remove code for change titlebar color in desktop version.

--- plugins/ssPanel.plugin.js
+++ plugins/ssPanel.plugin.js
@@ -12,20 +12,11 @@ ssPlugin.prototype.start = function () {
         if (e.ctrlKey && e.keyCode === 72 /* e.key === 'h' */)
             $("body").toggleClass("compact-mode");
     };
-    this.timerMLight = setInterval(function(){
-        if (($("body .theme-light").length > 0) !==
-            ($("body.theme-mLight").length > 0))
-        {
-            $("body").toggleClass("theme-mLight");
-        }
-    }, 1000);
     $(document).bind('keydown', this.ssHandler);
 };
 
 ssPlugin.prototype.stop = function () {
     $(document).unbind('keydown', this.ssHandler);
-    clearInterval(this.timerMLight);
-    delete this.timerMLight;
     $("body").removeClass("compact-mode");
 };
 
@@ -37,5 +28,5 @@ ssPlugin.prototype.observer   = function () {};
 ssPlugin.prototype.getSettingsPanel = function () { return ""; };
 ssPlugin.prototype.getName          = function () { return "mLite theme js plugin"; };
 ssPlugin.prototype.getDescription   = function () { return "Добавляет хоткей (ctrl+h) скрытия панелей для mLite темы"; };
-ssPlugin.prototype.getVersion       = function () { return "0.1.4"; };
+ssPlugin.prototype.getVersion       = function () { return "0.1.5"; };
 ssPlugin.prototype.getAuthor        = function () { return "LH"; };
